c7d8c5143c13e1367147ba30e63df7c8e332cb8f
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-03-29 13:41:07 -0400

    working on creating a log in page

---
+++ lambdatimes/src/components/Login.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import PropTyes from 'prop-types';
+import {Button, Form, FormGroup, Input} from 'reactstrap';
+
+
+class LogIn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password:''
+        }
+    }
+
+handleInputChange = event => {
+    this.setState({ [event.target.name]: event.target.value})
+}
+
+handleLogInSubmit = event => {
+    const user = this.state.username;
+    localStorahe.setItem('user', user)
+    window.location.reload();
+}
+
+}
+
+render() {
+    return (
+      <div className='form-container'>
+      <Form>
+     <FormGroup>
+        <Input 
+            type='text'
+        placeholder='Username'
+        name='username'
+        value={this.state.username}
+        onChange={this.handleInputChange}
+        />
+     </FormGroup>
+
+     <FormGroup>
+        <Input 
+        type='password'
+        placeholder='Top Secret Password Here'
+        name='password'
+        value={this.state.password}
+        onChange={this.handleInputChange}
+        />
+     </FormGroup>
+     <Button>LogIn</Button>
+        </Form>
+      </div> 
+    
+    )}     
+
+export default LogIn;
